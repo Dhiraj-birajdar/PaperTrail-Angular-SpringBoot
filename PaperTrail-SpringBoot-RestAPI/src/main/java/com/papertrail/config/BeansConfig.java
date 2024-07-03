8c3e90fbca6b417319426a3c0eb7d673ae391f16
Dhiraj-birajdar <birajdar257@gmail.com>
2024-07-03 19:26:25 +0530

    Phase 1 complete dockerized application

--- PaperTrail-SpringBoot-RestAPI/src/main/java/com/papertrail/config/BeansConfig.java
+++ PaperTrail-SpringBoot-RestAPI/src/main/java/com/papertrail/config/BeansConfig.java
@@ -1,6 +1,7 @@
 package com.papertrail.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -19,12 +20,15 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class BeansConfig {
 
     private final UserDetailsService userDetailsService;
+    @Value("${app.cors.origins}:*") // todo :* not recommended on production
+    private List<String> allowedOrigins;
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
@@ -53,23 +57,23 @@ public class BeansConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200")); // todo add url to application.yml
+//        configuration.setAllowCredentials(true); // todo: uncomment on production and remove :*
+        configuration.setAllowedOrigins(allowedOrigins);
         configuration.setAllowedHeaders(
-                Arrays.asList(
-                        HttpHeaders.ORIGIN,
-                        HttpHeaders.ACCEPT,
-                        HttpHeaders.CONTENT_TYPE,
-                        HttpHeaders.AUTHORIZATION
+                Arrays.asList( "*" // todo don't use in production
+//                        HttpHeaders.ORIGIN,
+//                        HttpHeaders.ACCEPT,
+//                        HttpHeaders.CONTENT_TYPE,
+//                        HttpHeaders.AUTHORIZATION
                 )
         );
         configuration.setAllowedMethods(
-                Arrays.asList(
-                        "GET",
-                        "POST",
-                        "PUT",
-                        "DELETE",
-                        "PATCH"
+                Arrays.asList( "*" // todo don't use in production
+//                        "GET",
+//                        "POST",
+//                        "PUT",
+//                        "DELETE",
+//                        "PATCH"
                 )
         );
         source.registerCorsConfiguration("/**", configuration);
